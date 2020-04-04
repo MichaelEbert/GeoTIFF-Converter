8ab6327505ab0b0ed31d78e577c02348309d94d0
Michael Ebert <mebert@pacbell.net>
2020-04-04 11:13:29 -0700

    powershell module to convert GeoTIFF to STL

---
+++ ConvertCmdlet.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using OSGeo.GDAL;
+using System.Numerics;
+
+namespace GeoTIFFConverter
+{
+    [Cmdlet(VerbsData.Convert, "GeoTIFF")]
+    public class ConvertCmdlet : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Path;
+
+        [Parameter]
+        public string Destination;
+
+        [Parameter]
+        public SwitchParameter RenderFaces = false;
+
+        [Parameter]
+        public SwitchParameter Text = false;
+
+        [Parameter]
+        public double Scale = 0.1;
+
+        [Parameter]
+        public int MaxY = -1;
+
+        protected override void BeginProcessing()
+        {
+            GdalConfiguration.ConfigureGdal();
+            System.Console.WriteLine("Starting...");
+            Gdal.AllRegister();
+            if(Destination == null)
+            {
+                Destination = System.IO.Path.GetDirectoryName(Path) + "/Output.stl";
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            var dataset = Gdal.Open(Path, Access.GA_ReadOnly);
+            Driver drv = dataset.GetDriver();
+            Console.WriteLine("number of things is:");
+            Console.WriteLine(dataset.RasterCount);
+            //band numbers start at 1
+            var heightdata = dataset.GetRasterBand(1);
+            double[] minmax = new double[2];
+            heightdata.ComputeRasterMinMax(minmax, 0);
+            Console.WriteLine($"minmax is {minmax[0]}, {minmax[1]}");
+
+            //create a buffer to hold data in memory.
+            int maxX = heightdata.XSize;
+            if (MaxY == -1)
+            {
+                MaxY = heightdata.YSize;
+            }
+
+            double[] databuffer = new double[maxX * MaxY];
+            heightdata.ReadRaster(0, 0, maxX, MaxY, databuffer, maxX, MaxY, 0, 0);
+            //heightdata.ReadRaster()
+            //misc stuff
+            heightdata.GetNoDataValue(out double nodataval, out int hasnodataval);
+            Console.WriteLine($"nodata vals are {nodataval}, {hasnodataval}");
+            var thing = heightdata.GetRasterColorInterpretation();
+            var thing2 = Gdal.GetDataTypeSize(heightdata.DataType);
+
+            using (var outfile = new STLExporter(Destination, !Text))
+            {
+                if (RenderFaces)
+                {
+                    for (int y = 0; y < MaxY - 1; ++y)
+                    {
+                        for (int x = 0; x < maxX - 1; ++x)
+                        {
+                            int index = y * maxX + x;
+                            double zvalue = databuffer[index];
+
+                            Vector3 pointA = new Vector3((float)(x * Scale),(float)(y * Scale), (float)(zvalue * Scale));
+                            Vector3 pointB = new Vector3((float)(x * Scale),(float)((y+1) * Scale), (float)(databuffer[index+maxX] * Scale));
+                            Vector3 pointC = new Vector3((float)((x+1) * Scale),(float)(y * Scale), (float)(databuffer[index+1] * Scale));
+                            Vector3 pointD = new Vector3((float)((x+1) * Scale),(float)((y+1) * Scale), (float)(databuffer[index+1+maxX] * Scale));
+                            outfile.WriteTriangle(pointA, pointB, pointC);
+                            outfile.WriteTriangle(pointB, pointC, pointD);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int y = 0; y < MaxY; ++y)
+                    {
+                        for (int x = 0; x < maxX; ++x)
+                        {
+                            int index = y * maxX + x;
+                            double zvalue = databuffer[index];
+                            outfile.WritePoint((float)(x * Scale), (float)(y * Scale), (float)(zvalue * Scale));
+                        }
+                    }
+                }
+            }
+            //if we want to do triangles, we need:
+            //x,y, x+1,y, x,y+1, x+1,y, x, y+1, x+1, y+1.
+            Console.WriteLine($"mend");
+        }
+
+    }
+}
---
+++ STLExporter.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTIFFConverter
+{
+    class STLExporter : IDisposable
+    {
+        bool binary = false;
+        FileStream file;
+        int triCount = 0;
+        int triCountOffset = 80;//byte 80
+
+        public STLExporter(string filename, bool binary = true)
+        {
+            this.binary = binary;
+            file = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            //header
+            if (binary)
+            {
+                byte[] bytes = new byte[80];
+                file.Write(bytes, 0, 80);
+                file.Write(bytes, 0, 4);
+            }
+            else
+            {
+                write("solid stlobject\n");
+            }
+        }
+
+        public void WritePoint(float x, float y, float z)
+        {
+            if (binary)
+            {
+                write(0.0F);
+                write(0.0F);
+                write(0.0F);
+                //point 1
+                write((float)x);
+                write((float)y);
+                write((float)z);
+                //point 2
+                write((float)x);
+                write((float)y);
+                write((float)z);
+                //point 3
+                write((float)x);
+                write((float)y);
+                write((float)z);
+                file.WriteByte(0);
+                file.WriteByte(0);
+                triCount++;
+            }
+            else
+            {
+                write("  facet normal 0 0 0\n");
+                write("    outer loop\n");
+                write($"      vertex {x} {y} {z}\n");
+                write($"      vertex {x} {y} {z}\n");
+                write($"      vertex {x} {y} {z}\n");
+                write("    endloop\n");
+                write("  endfacet\n");
+            }
+        }
+
+        public void WriteTriangle(Vector3 a, Vector3 b, Vector3 c)
+        {
+            if (binary)
+            {
+                write(0.0F);
+                write(0.0F);
+                write(0.0F);
+                write(a.X);
+                write(a.Y);
+                write(a.Z);
+                write(b.X);
+                write(b.Y);
+                write(b.Z);
+                write(c.X);
+                write(c.Y);
+                write(c.Z);
+                file.WriteByte(0);
+                file.WriteByte(0);
+                triCount++;
+            }
+            else
+            {
+                write("  facet normal 0 0 0\n");
+                write("    outer loop\n");
+                write($"      vertex {a.X} {a.Y} {a.Z}\n");
+                write($"      vertex {b.X} {b.Y} {b.Z}\n");
+                write($"      vertex {c.X} {c.Y} {c.Z}\n");
+                write("    endloop\n");
+                write("  endfacet\n");
+            }
+        }
+
+        private void write(int intToWrite)
+        {
+            byte[] bytes = BitConverter.GetBytes(intToWrite);
+            file.Write(bytes, 0, bytes.Length);
+        }
+        private void write(float floatToWrite)
+        {
+            byte[] bytes = BitConverter.GetBytes(floatToWrite);
+            file.Write(bytes, 0, bytes.Length);
+        }
+        private void write(string stringToWrite)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(stringToWrite);
+            file.Write(bytes, 0, bytes.Length);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (binary)
+                    {
+                        file.Seek(triCountOffset, SeekOrigin.Begin);
+                        write(triCount);
+                    }
+                    else
+                    {
+                        write("endsolid stlobject");
+                    }
+                    file.Close();
+                    file.Dispose();
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
